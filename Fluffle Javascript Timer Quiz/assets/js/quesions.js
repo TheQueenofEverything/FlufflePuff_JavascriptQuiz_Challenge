668adf0d59773e15d0e165d6b526c7ba88ac3dfb
Mariana Marques <65464431+TheQueenofEverything@users.noreply.github.com>
2022-01-18 17:45:55 +0100

    Add files via upload

---
+++ Fluffle Javascript Timer Quiz/assets/js/quesions.js
@@ -0,0 +1,40 @@
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include___",
+      choices: [ "strings", "alerts","booleans",  "numbers"],
+      answerIndex: 1
+    },
+    {
+      title: "The condition in an if/else statement is enclosed within___",
+      choices: [ "curly brackets","parenthesis","quotes",  "square brackets"],
+      answerIndex: 0
+    },
+    {
+      title: "Arrays in JavaScript can be used to store___",
+      choices: [
+        "numbers and strings", "other arrays", "booleans", "all of the above"],
+      answerIndex: 3
+    },
+    {
+      title:
+        "String values must be enclosed within_____when being assigned to variables.",
+      choices: ["commas", "quotes", "parenthesis", "none"],
+      answerIndex: 1
+    },
+    {
+      
+        title: "A very useful tool used during development add debugging for printing content to the debugger is___",
+      choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+      answerIndex: 3
+    },
+    {
+        title:"What does CSS stand for___",
+        choices: ["Creative Style Sheets", "Cascading Style Sheets", "Computer Style Sheets", "Colorful Style Sheets"],
+        answerIndex: 1
+    },
+    {
+        title:"Where in an HTML document is the correct place to refer to an external style sheet?",
+        choices: ["At the end of the document", "In the body section","In the head section",  "just before the footer section"],
+        answerIndex: 2
+    }
+  ];
